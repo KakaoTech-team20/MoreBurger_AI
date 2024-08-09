894f11352f8a2a5f7851bca3150d0564e0f7bcc1
happy <kimjohyun298@gmail.com>
2024-08-09 14:49:25 +0900

    jo commit

--- app/routers/recommend.py
+++ app/routers/recommend.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-
+# jo commit
 @router.get("/recommend/")
 async def get_recommendation():
     return {"status": 200, "items": ["item1", "item2", "item3"]}
