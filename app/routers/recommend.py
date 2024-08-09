2335be7ceda77c5e215ac3cfd3f87f2350722f3e
andrew <whdms1107@gmail.com>
2024-08-09 14:42:16 +0900

    ✨ feat: anrew commit

--- app/routers/recommend.py
+++ app/routers/recommend.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
+# andrew 작업물
+
 @router.get("/recommend/")
 async def get_recommendation():
     return {"status": 200, "items": ["item1", "item2", "item3"]}
