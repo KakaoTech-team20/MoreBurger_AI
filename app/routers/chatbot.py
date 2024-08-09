871d63182884a2288241210abfa1ccd46adc3fee
andrew <whdms1107@gmail.com>
2024-08-09 14:44:00 +0900

    ✨ feat: anrew commit

--- app/routers/chatbot.py
+++ app/routers/chatbot.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
+
+# chatbot andrew
 @router.get("/chatbot/")
 async def get_recommendation():
     return {"status": 200, "items": ["item1", "item2", "item3"]}
