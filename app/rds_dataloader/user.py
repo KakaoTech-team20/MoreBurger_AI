a022d05718a2904a240ac979445bc769bfc2716e
andrew <whdms1107@gmail.com>
2024-08-27 18:37:29 +0900

    💄 style: get_user_by_id의 return type을 pd.DataFrame 에서 pd.Series로 변경
    유저의 경우 1개의 결괏값만을 필요로 하기 때문에 DataFrame일 이유가 없음.

--- app/rds_dataloader/user.py
+++ app/rds_dataloader/user.py
@@ -2,10 +2,11 @@ import pandas as pd
 from .connection import db
 
 
-def get_user_by_id(user_id: int) -> pd.DataFrame:
+def get_user_by_id(user_id: int) -> pd.Series:
     """
-    입력받은 user_id를 이용해 RDS database에 존재하는 user의 행을 얻어오는 함수
-    :param user_id: 
-    :return: 유저 정보 또는 None (해당 ID의 유저가 없는 경우)
+    입력받은 user_id를 이용해 RDS database에 존재하는 user의 정보를 Series로 반환하는 함수
+    :param user_id: 조회할 사용자의 ID
+    :return: 사용자 정보가 담긴 Series 또는 None (해당 ID의 사용자가 없는 경우)
     """
-    return pd.DataFrame(db.execute_single_query("SELECT * FROM users WHERE id = %s", (user_id,)))
+    result = db.execute_single_query("SELECT * FROM users WHERE id = %s", (user_id,))
+    return pd.Series(result) if result else None
