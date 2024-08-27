2fd6ec62458ddabd0d47342a60de44cb6c310383
andrew <whdms1107@gmail.com>
2024-08-27 18:24:06 +0900

    ✨ feat: rds_dataloader의 검색결과 DataFrame 형태로 변경

--- app/rds_dataloader/user.py
+++ app/rds_dataloader/user.py
@@ -1,10 +1,11 @@
-from typing import Any, Dict
+import pandas as pd
 from .connection import db
 
-def get_user_by_id(user_id: int) -> Dict[str, Any] | None:
+
+def get_user_by_id(user_id: int) -> pd.DataFrame:
     """
     입력받은 user_id를 이용해 RDS database에 존재하는 user의 행을 얻어오는 함수
     :param user_id: 
     :return: 유저 정보 또는 None (해당 ID의 유저가 없는 경우)
     """
-    return db.execute_single_query("SELECT * FROM users WHERE id = %s", (user_id,))
+    return pd.DataFrame(db.execute_single_query("SELECT * FROM users WHERE id = %s", (user_id,)))
