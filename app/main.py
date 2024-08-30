7511d18d9e4a92763343cd8f0dbe5245919f229f
happy <kimjohyun298@gmail.com>
2024-08-30 12:17:59 +0900

    feat: 사용자 정보 기반의 챗봇 응답 생성

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
-# FastAPI 애플리케이션의 진입점 역할 + 각 엔드포인트 라우터를 애플리케이션에 연결하는 역할
+# FastAPI 애플리케이션의 진입점
+# 라우터를 포함하고 서버를 실행 시킨다
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from app.routers import recommend, chatbot
-from pydantic import BaseModel
-from app.model.chatbot_model import generate_response
+
+# FastAPI 애플리케이션(app) 초기화
 app = FastAPI()
 
 # 추천 시스템, 챗봇 엔드포인트를 FastAPI 애플리케이션에 연결
 app.include_router(recommend.router, prefix="/api")
+
+# 챗봇 경로에 대한 라우터 등록
 app.include_router(chatbot.router, prefix="/api")
 
 @app.get("/")
@@ -19,19 +22,6 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
-class ChatRequest(BaseModel):
-    message: str
-
-@app.post("/chatbot/")
-async def chat(request: ChatRequest):
-    try:
-        response = generate_response(request.prompt)
-        return {"response": response}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-
 # 실행
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
+    uvicorn.run(app, port=8000) # host = '127.0.0.1'
--- app/routers/chatbot.py
+++ app/routers/chatbot.py
@@ -1,18 +1,38 @@
+# 요청에 대한 응답 관련 코드
+# FastAPI 라우터와 엔드포인트를 정의해 요청을 처리하고 응답하는 역할
+import openai
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+
 from app.model.chatbot_model import get_chatbot_response
+from app.rds_dataloader import get_all_burgers
+from app.rds_dataloader.user import get_user_by_id
 
 router = APIRouter()
+
 
 # 사용자 메세지를 받기 위한 Pydantic 모델 정의
 class ChatRequest(BaseModel):
     message: str
 
-@router.post("/chatbot")
-async def chatbot(chat_request: ChatRequest):
+
+@router.post("/chatbot/{user_id}")
+async def chat(user_id: int, request: ChatRequest):
+    # 사용자 데이터 가져오기
+    user = get_user_by_id(user_id)
+    burger = get_all_burgers()
+
+    # 맞춤형 데이터 구성
+    user_info = (
+        f"사용자 {user['nickname']}에 대한 정보입니다 : "
+        f"맵기 선호도: {user['spicy']}, "
+        f"먹는 양: {user['capacity']}"
+    )
+
     try:
-        # 클라이언트로부터 받은 메세지를 OpenAI API에 전달
-        response = get_chatbot_response(chat_request.message)
+        full_prompt = f"{user_info}\n{request.message}"
+        response = get_chatbot_response(full_prompt)
+
         return {"response": response}
 
     except Exception as e:
