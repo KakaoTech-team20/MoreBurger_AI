20c112049da71c5eb2975072684350ed42bb0307
happy <kimjohyun298@gmail.com>
2024-09-05 15:55:43 +0900

    feat: 채팅 시작 api 생성 및 스레드 생성 로직 구조화

--- app/routers/chatbot.py
+++ app/routers/chatbot.py
@@ -5,6 +5,8 @@ from fastapi import APIRouter, HTTPException
 import openai
 from pydantic import BaseModel
 
+from app.model.chatbot_model import retrieve_assistant, create_conversation_with_assistant, \
+    start_conversation_with_assistant
 from app.rds_dataloader import get_all_burgers
 from dotenv import load_dotenv
 
@@ -19,117 +21,43 @@ client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 # 사용자 메세지를 받기 위한 Pydantic 모델 정의
 class ChatRequest(BaseModel):
-    user_id: int
+    user_email: int
     message: str
+    thread_id: str
 
+# 창이 열리면 불리는 api, 대화 시작 메세지 주면서 스레드 시작 (스레드 id도 전달하기)
+@router.get("/start/")
+async def startchat():
+    try:
+        vector_store, assistant = retrieve_assistant()
+        assistant_reply, thread_id = start_conversation_with_assistant(assistant)
+        # 이 부분은 항상 assistant_reply가 "무엇을 도와들리까요?"이다 -> 이렇게 하는 이유는, 스레드 id로 이어지는 대화를 가능하게 하기 위해
+        return assistant_reply, thread_id
+
+    except Exception as e:
+        print(f"Error starting conversation: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.post("/chatbot/")
 async def chat(request: ChatRequest):
     try:
         # 사용자 데이터 가져오기
-        user_id, message = request.user_id, request.message
-        print(f"User ID: {user_id}")
+        thread_id, user_email, message = request.user_email, request.message, request.thread_id
+        print(f"User Email: {user_email}")
         print(f"Message: {message}")
+        print(f"Thread ID: {thread_id}")
 
         burger_df = get_all_burgers()
         burger_df.to_csv("burger_data.txt", index=False)
 
-        # RDS에서 받아온 버거 정보를 csv 형태로 변환 후, openai 에서 assistant가 thread에서 사용할 file 로 구성
-        file = client.files.create(
-            file=open("burger_data.txt", "rb"),
-            purpose='assistants'
-        )
+        assistant_reply, thread_id = create_conversation_with_assistant(message, thread_id)
 
-        # 사용자 메세지를 포함하는 새로운 대화 스레드 생성
-        thread = client.beta.threads.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": "햄버거 추천해줘"
-                        },
-                    ],
-                }
-            ]
-        )
-
-        # 생성된 스레드와 assistant를 사용해 대화 진행 (어씨스턴트 호출)
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id='asst_JR7zBXnLYraebcyRmKXrKU1o',
-            tools=[{"type": "file_search"}]
-        )
-        print(run)
+        return {"assistant_reply": assistant_reply, "thread_id": thread_id}
 
     except Exception as e:
         print(f"An error occurred: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
-
-if __name__ == "__main__":
-    user_id, message = 1, "햄버거 추천해줘"
-
-    # 벡터 스토어에서 기존 Assistant 불러오기
-    vector_store = client.beta.vector_stores.retrieve("vs_wZWpgYaxPipa8tByxs2DSDNk")
-
-    # 기존 assistant를 업데이트 하는 코드
-    assistant = client.beta.assistants.retrieve("asst_JR7zBXnLYraebcyRmKXrKU1o")
-
-    # 새로운 대화 스레드 생성
-    thread = client.beta.threads.create(
-        messages=[
-            {
-                "role": "user",
-                "content": "'무엇을 도와드릴까요?' 라고만 말해",
-            }
-        ]
-    )
-
-    # 메세지를 스레드에 추가
-    message = client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=message
-    )
-
-    # assistant와 대화 실행
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant.id
-    )
-
-    # 대화가 완료될 때까지 상태 확인
-    while run.status != "completed":
-        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-        print(run.status)
-        print(run.completed_at)
-
-    # 대화가 완료되면 메세지를 불러옴
-    message = client.beta.threads.messages.list(thread_id=thread.id)
-    print("챗봇 : ", message.data[0].content[0].text.value)
-
-    # 사용자와의 대화를 반복해서 수행
-    while True:
-        message = input("인간 : ")
-        message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=message
-        )
-
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant.id
-        )
-
-        # 대화 완료까지 상태 확인
-        while run.status != "completed":
-            run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-            print(run.status)
-            print(run.completed_at)
-
-        # 대화 완료 후 챗봇 메시지를 출력
-        message = client.beta.threads.messages.list(thread_id=thread.id)
-        print(message.data[0].content[0].text.value)
+#
+# if __name__ == "__main__":
+#     user_id, message = 1, "햄버거 추천해줘"
